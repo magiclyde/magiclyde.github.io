912d26406b9b53a318e1a751d582489b81f777af
clyde chan <magiclyde@users.noreply.github.com>
2019-09-18 00:19:56 +0800

    Create redis_test.lua

---
+++ snippets/lua/redis_test.lua
@@ -0,0 +1,45 @@
+local redis = require "resty.redis"
+local red = redis:new()
+
+red:set_timeout(1000)
+
+local ok, err = red:connect("127.0.0.1", 6379)
+if not ok then
+    ngx.say("failed to connect: ", err)
+    return
+end
+
+-- 请注意这里 auth 的调用过程
+local count
+count, err = red:get_reused_times()
+if 0 == count then
+    ok, err = red:auth("password")
+    if not ok then
+        ngx.say("failed to auth: ", err)
+        return
+    end
+elseif err then
+    ngx.say("failed to get reused times: ", err)
+    return
+end
+
+ok, err = red:set("dog", "an animal")
+if not ok then
+    ngx.say("failed to set dog: ", err)
+    return
+end
+
+ngx.say("set result: ", ok)
+
+local res, err = red:get("dog")
+if not res then
+    ngx.say("failed to get dog: ", err)
+    return
+end
+
+if res == ngx.null then
+    ngx.say("dog not found.")
+    return
+end
+
+ngx.say("dog: ", res)
